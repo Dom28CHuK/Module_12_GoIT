55c8ac5ee5aa4bb7252a5e52ffa354e27f85e7c5
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-06-09 12:14:11 +0300

    u

--- src/java/multithreadingDemo/ChieldThread.java
+++
@@ -1,9 +0,0 @@
-package multithreadingDemo;
-
-public class ChieldThread extends Thread {
-
-    @Override
-    public void run() {
-        System.out.println("done " + Thread.currentThread().getName());
-    }
-}
