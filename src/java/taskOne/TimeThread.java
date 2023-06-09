2cf929d8f54f2bc38b76a45334190a3ee54db0ec
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-06-09 14:51:28 +0300

    add taskOne

--- src/java/taskOne/TimeThread.java
+++ src/java/taskOne/TimeThread.java
@@ -1,4 +1,24 @@
 package taskOne;
 
-public class TimeThread {
+import java.time.Duration;
+import java.time.Instant;
+
+public class TimeThread implements Runnable{
+    @Override
+    public void run() {
+        Instant startTime = Instant.now();
+
+        while (true) {
+            Instant currentTime = Instant.now();
+            Duration elapsedTime = Duration.between(startTime, currentTime);
+
+            System.out.println("Time has passed: " + elapsedTime.getSeconds() + " seconds");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
